3b7427bca83ee58a4b8045b0db148b22b2c5a30f
timlebrun111 <tdlebrun@email.neit.edu>
2022-01-11 18:36:07 -0500

    Add project files.

---
+++ Mathmatics.Test/BasicMathTest.cs
@@ -0,0 +1,51 @@
+﻿using Mathmatics2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Mathmatics.Test
+{
+    public class BasicMathTest
+    {
+        [Fact]
+        public void TestAddNumbers()
+        {
+            var math = new BasicMath();
+            var results =  math.AddNumbers(5,5);
+            Assert.True(results == 10);
+
+
+        }
+
+        [Fact]
+        public void TestSubtractNumbers()
+        {
+            var math = new BasicMath();
+            var results = math.SubtractNumbers(5, 5);
+            Assert.True(results == 0);
+
+
+        }
+
+        [Fact]
+        public void TestMultiplyNumbers()
+        {
+            var math = new BasicMath();
+            var results = math.MultiplyNumbers(5, 5);
+            Assert.True(results == 25);
+
+
+        }
+
+        [Fact]
+        public void TestDivideNumbers()
+        {
+            var math = new BasicMath();
+            var results = math.DivideNumbers(5, 5);
+            Assert.True(results == 1);
+
+
+        }
+    }
+}
---
+++ Mathmatics2/BasicMath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mathmatics2
+{
+    public class BasicMath
+    {
+        /// <summary>
+        /// This is a function that adds two numbers together
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public double AddNumbers(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double SubtractNumbers(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double MultiplyNumbers(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double DivideNumbers(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
